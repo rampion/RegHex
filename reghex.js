24e453a928cb4de189b5ce99fb8a11fc855321dc
Noah Luck Easterly <noah.easterly@gmail.com>
2013-02-16 21:32:45 -0500

    forming row associations

--- reghex.js
+++ reghex.js
@@ -1,5 +1,9 @@
-function loadPuzzle(puzzle) {
+// see the ids for each cell and edge
+function showIDs() {
+  $('[class~=edge],[class~=cell]').each(function(){ $('text', this).text(this.id); }); 
+}
 
+function loadPuzzle(puzzle) {
   // grab these to use as templates
   var $cell = $('#cells > g:first-child');
   var $edge = $('#edges > g:first-child');
@@ -11,6 +15,8 @@ function loadPuzzle(puzzle) {
   $('text', $edge).text('');
   $cell.attr('class', 'cell selectable');
   $edge.attr('class', 'edge');
+
+  function mkID(code, j, i){ return code+'-'+j+'-'+i; };
 
   // clone the templates to create a puzzle of the appropriate size
   for (var j = 0; j <= puzzle.edgeLen; j++){
@@ -28,18 +34,59 @@ function loadPuzzle(puzzle) {
 
       $donor.parent().append( $a, $b, $c );
 
-      // ids are used for the puzzle clues
-      $a.attr('id','A'+j+i);
-      $b.attr('id','B'+j+i);
-      $c.attr('id','C'+j+i);
+      $a.attr('id', mkID('A',j,i));
+      $b.attr('id', mkID('B',j,i));
+      $c.attr('id', mkID('C',j,i));
 
-      // some of the edge clues need to be rotated
       if (isEdge) {
+        // some of the edge clues need to be rotated
         $a.attr('transform', $a.attr('transform') + ' rotate(240)');
         $c.attr('transform', $c.attr('transform') + ' rotate(120)');
       }
     }
   }
+
+  // create rows of cells for each edge
+  $('[class~=cell]').each(function(){
+    $(this).data('edges', []);
+  });
+  $('[class~=edge][id]').each(function(){
+    var edge = this;
+    var m = edge.id.match(/^([ABC])-(\d+)-(\d+)$/);
+    var code = m[1];
+    var jMax = parseInt(m[2]);
+    var iMax = parseInt(m[3]);
+
+    var nextThird = {'A':'B','B':'C','C':'A'};
+    var prevThird = {'A':'C','B':'A','C':'B'};
+    var z = puzzle.edgeLen;
+
+    var cells = [];
+    function push(code,j,i){ 
+      cells.push( 
+        i == 0 && j == 0 
+          ? $cell 
+          : document.getElementById(mkID(code,j,i))
+      );
+    }
+
+    var i, j;
+    // some number from the same prefix (decrease by (1,1))
+    for (i = iMax - 1, j = jMax - 1; i > 0 && j >= 0; i--, j--) {
+      push( code, j, i );
+    }
+
+    if (jMax > iMax) for (var k = 0; k < z; k++) {
+      push( nextThird[code], k, jMax - iMax );
+    } else for (var k = 1; k < z; k++) {
+      push( prevThird[code], iMax - jMax, k );
+    }
+
+    $(edge).data('cells', cells);
+    cells.forEach(function(cell){ 
+      $(cell).data('edges').push(edge); 
+    });
+  });
 
   // load puzzle clues into the cells and edges
   for (var id in puzzle.clues) {
@@ -49,7 +96,8 @@ function loadPuzzle(puzzle) {
     $('text', $g).text(puzzle.clues[id]);
   }
 }
-function loadUI(){
+
+function loadUI() {
   // disable the buttons during rotation
   var rotationEnabled = true;
   $('animateTransform').on('beginEvent', function(){ rotationEnabled = false; });
@@ -69,7 +117,7 @@ function loadUI(){
       });
       rotation += delta;
     }
-  };
+  }
 
   // click the buttons at the top of the page to rotate the grid
   $('#widdershins,#clockwise').click(function(e){
@@ -78,16 +126,19 @@ function loadUI(){
   });
 
   // click to select a cell
-  $('#cells').on('click', '[class="cell selectable"]', function(e){
-    $('[class="cell selected"]').attr('class', 'cell selectable');
+  $('#cells').on('click', '[class~="selectable"]', function(e){
+    $('[class~="selected"]').attr('class', 'cell selectable');
     this.setAttribute('class', 'cell selected');
     e.stopPropagation();
   });
 
   // click away to clear selection
   $(document).click(function(e){
-    $('[class="cell selected"]').attr('class', 'cell selectable');
+    $('[class~="selected"]').attr('class', 'cell selectable');
   });
+
+  function check(cell){
+  };
 
   // use keys to
   //  * enter text
@@ -95,26 +146,26 @@ function loadUI(){
   //  * finish selection
   //  * rotate grid
   $(document).keypress(function(e){
-    var selected = $('[class="cell selected"]')[0];
+    var selected = $('[class~="selected"]')[0];
     var selected_text = selected ? $('text', selected)[0] : null;
 
     switch (e.keyCode){
-    case 27: /* escape */
-    case 13: /* enter */
+    case 27: // escape
+    case 13: // enter
       if (selected) {
         selected.setAttribute('class', 'cell selectable');
         return false;
       }
-    case 37: /* left */   
+    case 37: // left
       rotate('widdershins'); 
       return false;
-    case 39: /* right */  
+    case 39: // right
       rotate('clockwise'); 
       return false;
-    case  8: /* backspace */ 
+    case  8: // backspace
       if (selected_text) {
         selected_text.textContent = '';
-        return false;
+        check(this);
       }
     case 0: 
       if (selected_text) {
@@ -126,7 +177,7 @@ function loadUI(){
         if (!c) return;
         t = selected_text.textContent;
         selected_text.textContent = (t.indexOf(c) >= 0) ? t.replace(c,'') : t + c;
-        return false;
+        check(this);
       }
     }
   });
