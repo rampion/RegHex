df86bcb79df2b0179e912db9dbd53ffb207cd6f9
Noah Luck Easterly <noah.easterly@gmail.com>
2013-02-19 22:56:35 -0500

    got rid of ids

--- reghex.js
+++ reghex.js
@@ -22,109 +22,113 @@ $.fn.removeClassSVG = function(klasses){
 }
 
 // see the ids for each cell and edge
-function showCoords(){
-  $('[class~=edge] text,[class~=cell] text').text(function(){ 
+function showCoords($svg){
+  $('[class~=edge] text,[class~=cell] text', $svg).text(function(){ 
     return $(this).data('coords');
   }).css('font-size', '7pt');
 }
 
-function loadPuzzle(puzzle) {
-  // clear anything prior
-  $('#cells').empty();
-  $('#edges').empty();
+function loadPuzzleInto($svg) { 
+  function loadPuzzle(puzzle) {
+    // clear anything prior
+    $('[class~=cells]', $svg).empty();
+    $('[class~=edges]', $svg).empty();
 
-  if (puzzle.edgeLen <= 0) return;
-  
-  var dX = 10*Math.sqrt(3),
-      dY = 10,
-      $template = {
-        cell: $('#cell-template'),
-        edge: $('#edge-template')
-      },
-      $parent = {
-        cell: $('#cells'),
-        edge: $('#edges')
-      },
-      neighbors = [ 
-        { to: '-A', from: '+A', dA: -2, dB: +1, dC: +1 },
-        { to: '+A', from: '-A', dA: +2, dB: -1, dC: -1 },
-        { to: '-B', from: '+B', dA: +1, dB: -2, dC: +1 },
-        { to: '+B', from: '-B', dA: -1, dB: +2, dC: -1 },
-        { to: '-C', from: '+C', dA: +1, dB: +1, dC: -2 },
-        { to: '+C', from: '-C', dA: -1, dB: -1, dC: +2 },
-      ],
-      hexes = {};
+    if (puzzle.edgeLen <= 0) return;
+    
+    var dX = 10*Math.sqrt(3),
+        dY = 10,
+        $template = {
+          cell: $('defs [class~=cell]', $svg),
+          edge: $('defs [class~=edge]', $svg)
+        },
+        $parent = {
+          cell: $('[class~=cells]', $svg),
+          edge: $('[class~=edges]', $svg)
+        },
+        neighbors = [ 
+          { to: '-A', from: '+A', dA: -2, dB: +1, dC: +1 },
+          { to: '+A', from: '-A', dA: +2, dB: -1, dC: -1 },
+          { to: '-B', from: '+B', dA: +1, dB: -2, dC: +1 },
+          { to: '+B', from: '-B', dA: -1, dB: +2, dC: -1 },
+          { to: '-C', from: '+C', dA: +1, dB: +1, dC: -2 },
+          { to: '+C', from: '-C', dA: -1, dB: -1, dC: +2 },
+        ],
+        hexes = {};
 
-  function create(type, a, b, c, rotate){
-    var h = $template[type].clone(),
-        x = [a,b,c].join(','),
-        t = h.find('text');
+    function create(type, a, b, c, rotate){
+      var h = $template[type].clone(),
+          x = [a,b,c].join(','),
+          t = h.find('text');
 
-    // place the hex in the correct position
-    h.attr({
-      transform: 'translate('+(dX*b)+','+(dY*(c-a))+')' + (type == 'edge' ? ' rotate('+rotate+')' : ''),
-    });
-    $parent[type].append(h);
+      // place the hex in the correct position
+      h.attr({
+        transform: 'translate('+(dX*b)+','+(dY*(c-a))+')' + (type == 'edge' ? ' rotate('+rotate+')' : ''),
+      });
+      $parent[type].append(h);
 
-    h.find('text').data('coords', x); // for showCoords
+      h.find('text').data('coords', x); // for showCoords
 
-    // note down its adjacencies
-    hexes[x] = h;
-    neighbors.forEach(function(o){
-      var n = hexes[ [a+o.dA,b+o.dB,c+o.dC].join(',') ];
-      if (n) {
-        h.data( o.to,   n );
-        n.data( o.from, h );
+      // note down its adjacencies
+      hexes[x] = h;
+      neighbors.forEach(function(o){
+        var n = hexes[ [a+o.dA,b+o.dB,c+o.dC].join(',') ];
+        if (n) {
+          h.data( o.to,   n );
+          n.data( o.from, h );
+        }
+      });
+
+      // fill in the blanks
+      if (puzzle.clues[x]){
+        h.addClassSVG('clue').
+          removeClassSVG('selectable').
+          find('text').text(puzzle.clues[x]);
       }
-    });
-
-    // fill in the blanks
-    if (puzzle.clues[x]){
-      h.addClassSVG('clue').
-        removeClassSVG('selectable').
-        find('text').text(puzzle.clues[x]);
     }
+
+    // create all 1 + z*(z-1)*(z-1)/2 cells
+    //        and 3 + 6*(z-1) edges
+    var z = puzzle.edgeLen;
+    create('cell', 0, 0, 0);
+    create('edge', -2*z, z, z, 240 );
+    create('edge', z, -2*z, z, 0   );
+    create('edge', z, z, -2*z, 120 );
+    for (var k = 1; k < z; k++){
+      for (var t = 0; t < k; t++){
+        create('cell', +2*k-t, -k+2*t, -k-t);
+        create('cell', -2*k+t, +k-2*t, +k+t);
+        create('cell', -k-t, +2*k-t, -k+2*t);
+        create('cell', +k+t, -2*k+t, +k-2*t);
+        create('cell', -k+2*t, -k-t, +2*k-t);
+        create('cell', +k-2*t, +k+t, -2*k+t);
+      }
+      create('edge', -2*z+k, z-2*k, z+k, 240 );
+      create('edge', -2*z+k, z+k, z-2*k, 240 );
+      create('edge', z+k, -2*z+k, z-2*k, 0 );
+      create('edge', z-2*k, -2*z+k, z+k, 0 );
+      create('edge', z-2*k, z+k, -2*z+k, 120 );
+      create('edge', z+k, z-2*k, -2*z+k, 120 );
+    } 
   }
-
-  // create all 1 + z*(z-1)*(z-1)/2 cells
-  //        and 3 + 6*(z-1) edges
-  var z = puzzle.edgeLen;
-  create('cell', 0, 0, 0);
-  create('edge', -2*z, z, z, 240 );
-  create('edge', z, -2*z, z, 0   );
-  create('edge', z, z, -2*z, 120 );
-  for (var k = 1; k < z; k++){
-    for (var t = 0; t < k; t++){
-      create('cell', +2*k-t, -k+2*t, -k-t);
-      create('cell', -2*k+t, +k-2*t, +k+t);
-      create('cell', -k-t, +2*k-t, -k+2*t);
-      create('cell', +k+t, -2*k+t, +k-2*t);
-      create('cell', -k+2*t, -k-t, +2*k-t);
-      create('cell', +k-2*t, +k+t, -2*k+t);
-    }
-    create('edge', -2*z+k, z-2*k, z+k, 240 );
-    create('edge', -2*z+k, z+k, z-2*k, 240 );
-    create('edge', z+k, -2*z+k, z-2*k, 0 );
-    create('edge', z-2*k, -2*z+k, z+k, 0 );
-    create('edge', z-2*k, z+k, -2*z+k, 120 );
-    create('edge', z+k, z-2*k, -2*z+k, 120 );
-  } 
+  return loadPuzzle;
 }
 
-function loadUI() {
+function loadUI($svg) {
   // disable the buttons during rotation
   var rotationEnabled = true;
-  $('animateTransform').on('beginEvent', function(){ rotationEnabled = false; });
-  $('animateTransform').on('endEvent', function(){ rotationEnabled = true; });
+  $('animateTransform', $svg).on('beginEvent', function(){ rotationEnabled = false; });
+  $('animateTransform', $svg).on('endEvent', function(){ rotationEnabled = true; });
 
   // rotate clockwise or widdershins with a click of the buttons
   var rotation = 0;
   function rotate(direction){
     if (rotationEnabled){
+      console.log(direction);
       var delta = (direction == 'clockwise') ? +120 : -120;
-      $('animateTransform').each(function(){
+      $('animateTransform', $svg).each(function(){
         // the wheel and the cars rotate in opposite directions to keep the text facing up.
-        var sign = (this.id == 'wheel') ? +1 : -1;
+        var sign = $(this).is('[class~=wheel]') ? +1 : -1;
         this.setAttribute('from', sign * rotation);
         this.setAttribute('to', sign * (rotation + delta));
         this.beginElement();
@@ -134,21 +138,21 @@ function loadUI() {
   }
 
   // click the buttons at the top of the page to rotate the grid
-  $('#widdershins,#clockwise').click(function(e){
+  $('[class~=widdershins],[class~=clockwise]', $svg).click(function(e){
     e.preventDefault();
-    rotate(this.id);
+    rotate(this.getAttribute('class'));
   });
 
   // click to select a cell
-  $('#cells').on('click', '[class~="selectable"]', function(e){
-    $('[class~="selected"]').removeClassSVG('selected');
+  $('[class~=cells]', $svg).on('click', '[class~="selectable"]', function(e){
+    $('[class~="selected"]', $svg).removeClassSVG('selected');
     $(this).addClassSVG('selected');
     e.stopPropagation();
   });
 
   // click away to clear selection
-  $(document).click(function(e){
-    $('[class~="selected"]').removeClassSVG('selected');
+  $svg.click(function(e){
+    $('[class~="selected"]', $svg).removeClassSVG('selected');
   });
 
   // see whether this cell is part of any full words,
@@ -167,22 +171,23 @@ function loadUI() {
 
       // find the row text
       while ((c = c.data('+'+dim)) && c.is('[class~=cell]'))
-        cells.push(c[0]);
+        cells.push(c);
 
       var str = cells.map(function(cell){ return $('text', cell).text(); }).join('');
+      var $cells = $(cells).map(function(){ return this.toArray(); });
 
-      if (str.length < cells.length) {
+      if (str.length < $cells.length) {
         // incomplete
-        $(cells).removeClassSVG(success);
-        $(cells).removeClassSVG(failure);
+        $cells.removeClassSVG(success);
+        $cells.removeClassSVG(failure);
       } else if (str.match(regex)) {
         // success
-        $(cells).addClassSVG(success);
-        $(cells).removeClassSVG(failure);
+        $cells.addClassSVG(success);
+        $cells.removeClassSVG(failure);
       } else {
         // failure
-        $(cells).addClassSVG(failure);
-        $(cells).removeClassSVG(success);
+        $cells.addClassSVG(failure);
+        $cells.removeClassSVG(success);
       }
     });
   }
@@ -193,8 +198,9 @@ function loadUI() {
   //  * finish selection
   //  * rotate grid
   //  * move between hexes
-  $(document).keypress(function(e){
-    var $selected = $('[class~="selected"]');
+  $svg.parent().keypress(function(e){
+    if (e.altKey || e.ctrlKey || e.metaKey) return;
+    var $selected = $('[class~="selected"]', $svg);
 
     switch (e.keyCode){
     case 27: // escape
